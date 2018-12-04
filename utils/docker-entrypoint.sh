d80612291d9679cc73b19a2f2fedeefc341c33c4
Phil-Bastian Berndt <me@pbb.io>
2018-12-04 17:32:35 +0100

    add the SSH key stored in SSH_PRIVATE_KEY variable to the agent store

--- utils/docker-entrypoint.sh
+++ utils/docker-entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Run ssh-agent in current environment.
 eval $(ssh-agent -s)
+
+# Add the SSH key stored in SSH_PRIVATE_KEY variable to the agent store.
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
